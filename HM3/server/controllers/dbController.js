9e6a0b6c5535f879a97f86fff1cb13621a63b898
Khairy Krayyem <khairy.krayyem.2002@gmail.com>
2025-07-11 12:45:23 +0300

    task api and routes

--- HM3/server/controllers/dbController.js
+++ HM3/server/controllers/dbController.js
@@ -1,42 +1,50 @@
 const Task = require('../models/Task');
 
+// שליפת כל המשימות
 async function getTasks(req, res) {
-  // TODO
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch tasks" });
+  }
 }
 
+// הוספת משימה חדשה
 async function addTask(req, res) {
-  // TODO
+  try {
+    const { title } = req.body;
+    const newTask = new Task({ title });
+    await newTask.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).json({ error: "Failed to add task" });
+  }
 }
 
+// החלפת מצב המשימה
 async function toggleTask(req, res) {
-  // TODO
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ error: "Task not found" });
+
+    task.done = !task.done;
+    await task.save();
+    res.json(task);
+  } catch (err) {
+    res.status(400).json({ error: "Failed to toggle task" });
+  }
 }
 
+// מחיקת משימה
 async function deleteTask(req, res) {
-  // TODO
-}
-
-module.exports = {
-  getTasks,
-  addTask,
-  toggleTask,
-  deleteTask,
-};const Task = require('../models/Task');
-
-async function getTasks(req, res) {
-  // TODO
-}
-
-async function addTask(req, res) {
-  // TODO
-}
-
-async function toggleTask(req, res) {
-  // TODO
-}
-
-async function deleteTask(req, res) {
-  // TODO
+  try {
+    const deleted = await Task.findByIdAndDelete(req.params.id);
+    if (!deleted) return res.status(404).json({ error: "Task not found" });
+    res.json({ message: "Task deleted" });
+  } catch (err) {
+    res.status(400).json({ error: "Failed to delete task" });
+  }
 }
 
 module.exports = {
--- HM3/server/routes/api.js
+++ HM3/server/routes/api.js
@@ -1,12 +1,15 @@
-// Require express and create a router
+const express = require('express');
+const router = express.Router();
+const {
+  getTasks,
+  addTask,
+  toggleTask,
+  deleteTask,
+} = require('../controllers/dbController');
 
-// Require your controller
-// const db = ...
+router.get('/tasks', getTasks);
+router.post('/tasks', addTask);
+router.patch('/tasks/:id', toggleTask);
+router.delete('/tasks/:id', deleteTask);
 
-// Wire each of the following routes to its controller method:
-// GET    /tasks         => db.getTasks
-// POST   /tasks         => db.addTask
-// PATCH  /tasks/:id     => db.toggleTask
-// DELETE /tasks/:id     => db.deleteTask
-
-// Export the router
+module.exports = router;
